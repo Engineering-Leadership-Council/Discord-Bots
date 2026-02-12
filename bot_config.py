94e6e81a22e9e76ee7452523f25fa7467fdc5388
Connor The Maker <connortmaker@gmail.com>
2026-02-12 00:31:55 -0500

    Edited bot_config

--- bot_config.py
+++ bot_config.py
@@ -3,7 +3,7 @@
 # Role Bot
 ENABLE_ROLE_BOT = True
 ROLE_BOT_NICKNAME = "Sudo Master"
-ROLE_BOT_FOOTER = "Sudo Master • Reaction Roles"
+ROLE_BOT_FOOTER = "Obtain your roles!"
 
 # Welcome Bot
 ENABLE_WELCOME_BOT = True
@@ -12,4 +12,4 @@ WELCOME_BOT_NICKNAME = "Jeff the Doorman"
 # Event Bot
 ENABLE_EVENT_BOT = True
 EVENT_BOT_NICKNAME = "The Event Loop"
-EVENT_BOT_FOOTER = "The Event Loop"
+EVENT_BOT_FOOTER = "Notifying you of upcoming events!"
